4b7779119e1941f8d6bd959d130e9511ebf58af4
py073600 <prashant.yadav@citymall.live>
2025-03-08 14:52:51 +0530

    server changes

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.get('*', (req, res) => {
     res.redirect('/');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
